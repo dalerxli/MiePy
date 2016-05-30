c0aff75a9fee74163096d3ed4d9ca7088e35846a
John <japarker@uchicago.edu>
2016-05-29 20:49:57 -0500

    can plot materials; added drude-lorentz for eps

--- miepy/materials.py
+++ miepy/materials.py
@@ -1,8 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import miepy
+from scipy import constants
+
+
 
 class material:
-    """Contains eps and mu as function of wavelength"""
+    """Contains eps and mu (both complex) as function of wavelength"""
     def __init__(self,wav,eps,mu):
         self.Nfreq = len(wav)
         self.wav = wav
@@ -10,6 +14,16 @@ class material:
         self.mu = mu
         self.k = 2*np.pi/wav
 
+def plot_material(mat):
+    """Plot material properties of 'mat' """
+    plt.plot(mat.wav, mat.eps.real, linewidth=2, label="eps real")
+    plt.plot(mat.wav, mat.eps.imag, linewidth=2, label="eps imag")
+    plt.plot(mat.wav, mat.mu.real, linewidth=2, label="mu real")
+    plt.plot(mat.wav, mat.mu.imag, linewidth=2, label="mu imag")
+    plt.xlabel("Wavelength (nm)")
+    plt.ylabel("eps/mu")
+    plt.legend()
+    
 
 def load_material(filename):
     """Given a filename containing the following columns (separated by any whitespace, and the last two columns optional):
@@ -43,6 +57,35 @@ def save_material(filename, mat):
                     mat.mu.real, mat.mu.imag]).T
     miepy.array_io.save(filename, out)
 
+
+
+def drude_lorentz(wp, sig, om, gam, wav):
+    """Create a material using a Drude-Lorentz function
+            wp    =  plasma frequency
+            sig   =  strength factors
+            om    =  resonant frequencies
+            gam   =  damping factors
+            wav   =  wavelengths"""
+
+    Nfreq = len(wav)
+    size = len(sig)
+    omega = 2*np.pi*constants.c*constants.hbar/(constants.e*wav*1e-9)
+
+    eps = np.ones(Nfreq, dtype=np.complex)
+    mu = np.ones(Nfreq, dtype=np.complex)
+    for i in range(size):
+        eps_add = sig[i]*wp**2/(om[i]**2 - omega**2 -1j*omega*gam[i])
+        eps += eps_add
+
+    return material(wav,eps,mu)
+
 def Ag():
     """Return silver material from MiePy Ag data"""
-    return load_material(miepy.__path__[0] + "/materials/ag.npy")
+    # return load_material(miepy.__path__[0] + "/materials/ag.npy")
+
+    wp = 9.01
+    sig = [1.01889808, 0.62834151]
+    f = [0,5.05635462]
+    gam  = [0.01241231, 0.54965831]
+    wav = np.linspace(300,1100,1000)
+    return drude_lorentz(wp,sig,f,gam,wav)
