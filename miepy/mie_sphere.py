80f6056266aa3d2009decd34adaee767ed9f2e06
John <japarker@uchicago.edu>
2017-01-31 00:31:32 -0600

    beginning additions for field calculations

--- miepy/mie_sphere.py
+++ miepy/mie_sphere.py
@@ -3,9 +3,10 @@ mie_sphere calculates the scattering coefficients of a sphere using Mie theory
 """
 import numpy as np
 from miepy.scattering import multipoles
-from miepy.special_functions import riccati_1,riccati_2
+from miepy.special_functions import riccati_1,riccati_2,vector_spherical_harmonics
 
-def sphere(nmax, mat, r, eps_b=1, mu_b=1):
+class sphere
+    def __init__(self, nmax, mat, r, eps_b=1, mu_b=1):
     """Determine an, bn coefficients for a sphere
 
            nmax  = maximum number of orders to use
@@ -16,24 +17,67 @@ def sphere(nmax, mat, r, eps_b=1, mu_b=1):
 
        Returns multipoles object"""
 
-    n_b = (eps_b)**.5
-    xvals = mat.k*r*n_b
+        self.n_b = (eps_b)**.5
+        self.mat = mat
+        self.r = r
+        self.eps_b = eps_b
+        self.mu_b = mu_b
+        self.nmax = nmax
 
-    an = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
-    bn = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
+        self.an = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
+        self.bn = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
+        self.cn = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
+        self.dn = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
 
-    for i,x in enumerate(xvals):
-        m = (mat.eps[i]/eps_b)**.5
-        mt = m*mu_b/mat.mu[i]
-
-        jn,jn_p = riccati_1(nmax,x)
-        jnm,jnm_p = riccati_1(nmax,m*x)
-        yn,yn_p = riccati_2(nmax,x)
-        a = (mt*jnm*jn_p - jn*jnm_p)/(mt*jnm*yn_p - yn*jnm_p)
-        b = (jnm*jn_p - mt*jn*jnm_p)/(jnm*yn_p - mt*yn*jnm_p)
-        an[:,i] = a[1:]
-        bn[:,i] = b[1:]
+        self.exterior_computed = False
+        self.interior_computed = False
     
-    an = np.nan_to_num(an)
-    bn = np.nan_to_num(bn)
-    return multipoles(mat.wav,an,bn) 
+    def scattering(self):
+        xvals = self.mat.k*self.r*self.n_b
+        for i,x in enumerate(xvals):
+            m = (self.mat.eps[i]/self.eps_b)**.5
+            mt = m*self.mu_b/self.mat.mu[i]
+
+            jn,jn_p = riccati_1(self.nmax,x)
+            jnm,jnm_p = riccati_1(self.nmax,m*x)
+            yn,yn_p = riccati_2(self.nmax,x)
+            a = (mt*jnm*jn_p - jn*jnm_p)/(mt*jnm*yn_p - yn*jnm_p)
+            b = (jnm*jn_p - mt*jn*jnm_p)/(jnm*yn_p - mt*yn*jnm_p)
+            self.an[:,i] = a[1:]
+            self.bn[:,i] = b[1:]
+        
+        self.an = np.nan_to_num(self.an)
+        self.bn = np.nan_to_num(self.bn)
+
+        self.exterior_computed = True
+        return multipoles(mat.wav,self.an,self.bn) 
+
+    def compute_cd(self):
+        xvals = self.mat.k*self.r*self.n_b
+        for i,x in enumerate(xvals):
+            m = (self.mat.eps[i]/self.eps_b)**.5
+            mt = m*self.mu_b/self.mat.mu[i]
+
+            jn,jn_p = riccati_1(self.nmax,x)
+            jnm,jnm_p = riccati_1(self.nmax,m*x)
+            yn,yn_p = riccati_2(self.nmax,x)
+
+            c = (m*jn*yn_p - m*yn*jn_p)/(jnm*yn_p - mt*yn*jnm_p)
+            d = (m*jn*yn_p - m*yn*jn_p)/(mt*jnm*yn_p - yn*jnm_p)
+            self.cn[:,i] = c[1:]
+            self.dn[:,i] = d[1:]
+        
+        self.cn = np.nan_to_num(self.cn)
+        self.dn = np.nan_to_num(self.dn)
+
+        self.interior_computed = True
+
+    def E_field(self, k_index, Nmax = None):
+        if not Nmax: Nmax = self.nmax
+
+        def E(r, theta, phi):
+            for n in range(1,Nmax+1):
+                VSH = vector_spherical_harmonics(n,1)
+                En = 1j**n*(2n+1)/(n*(n+1))
+
+
--- miepy/special_functions.py
+++ miepy/special_functions.py
@@ -5,6 +5,9 @@ special_functions defines any additional special functions needed by MiePy
 import numpy as np
 from scipy import special
 
+def spherical_hn(n, z, derivative=False):
+    return special.spherical_jn(n,z,derivative) + 1j*special.spherical_yn(n,z,derivative)
+
 def riccati_1(nmax,x):
     """Riccati bessel function of the 1st kind
 
@@ -15,14 +18,6 @@ def riccati_1(nmax,x):
     r0 = x*jn
     r1 = jn + x*jnp
     return np.array([r0,r1])
-
-def riccati_1_single(n,x):
-    """Riccati_1, but only a single n value"""
-    pre = (np.pi*x/2)**.5
-    jn = pre*special.jv(n+0.5,x)
-    jnp = jn/(2*x) + pre*special.jvp(n+0.5,x)
-
-    return np.array([jn,jnp])
 
 def riccati_2(nmax,x):
     """Riccati bessel function of the 2nd kind
@@ -37,15 +32,6 @@ def riccati_2(nmax,x):
     r1 = hn + x*hnp
     return np.array([r0,r1])
 
-
-def riccati_2_single(n,x):
-    """Riccati_2, but only a single n value"""
-    pre = (np.pi*x/2)**.5
-    hn = pre*special.hankel1(n+0.5,x)
-    hnp = hn/(2*x) + pre*special.h1vp(n+0.5,x)
-
-    return np.array([hn,hnp])
-
 def riccati_3(nmax,x):
     """Riccati bessel function of the 3rd kind
 
@@ -57,6 +43,84 @@ def riccati_3(nmax,x):
     r1 = yn + x*ynp
     return np.array([r0,r1])
 
+def pi_tau_func(n):
+    # if np.sin(theta) == 0: return 0
+    lpn = special.legendre(n)
+    lpn_p = lpn.deriv()
+
+    def pi_func(theta):
+        with np.errstate(divide='ignore', invalid='ignore'):
+            pi_func = lpn(np.cos(theta))/np.sin(theta)
+            pi_func[pi_func == np.inf] = 0
+            pi_func = np.nan_to_num(pi_func)
+
+    def tau_func(theta):
+        tau_func = -1*np.sin(theta)*lpn_p(np.cos(theta))
+
+    return pi_func, tau_func 
+
+class vector_spherical_harmonics:
+    def __init__(self, n, superscript=1):
+        self.pi_func, self.tau_func = pi_tau_func(n)
+        self.n = n
+
+        if superscript == 1:
+            self.z_func = lambda x: special.spherical_jn(n,x)
+            self.zp_func = lambda x: special.spherical_jn(n,x, derivative=True)
+        elif superscript == 3:
+            self.z_func = lambda x: spherical_hn(n,x)
+            self.zp_func = lambda x: spherical_hn(n,x, derivative=True)
+
+    def M_o1n(self, k):
+        def f(r, theta, phi):
+            theta_comp = np.cos(phi)*self.pi_func(theta)*self.z_func(k*r)
+            phi_comp = -1*np.sin(phi)*self.tau_func(theta)*self.z_func(k*r)
+            r_comp = np.zeros(shape = theta.shape, dtype=np.complex)
+            return np.array([r_comp, theta_comp, phi_comp])
+        return f
+
+    def M_e1n(self, k):
+        def f(r, theta, phi):
+            theta_comp = -1*np.sin(phi)*self.pi_func(theta)*self.z_func(k*r)
+            phi_comp = -1*np.cos(phi)*self.tau_func(theta)*self.z_func(k*r)
+            r_comp = np.zeros(shape = theta.shape, dtype=np.complex)
+            return np.array([r_comp, theta_comp, phi_comp])
+        return f
+
+    def N_o1n(self, k):
+        def f(r, theta, phi):
+            p = k*r
+            theta_comp = np.sin(phi)*self.tau_func(theta)*(self.z_func(p) + p*self.zp_func(p))/p
+            phi_comp = np.cos(phi)*self.pi_func(theta)*(self.z_func(p) + p*self.zp_func(p))/p
+            r_comp = np.sin(phi)*self.n*(self.n+1)*np.sin(theta)*self.pi_func(theta)*self.z_func(p)/p
+            return np.array([r_comp, theta_comp, phi_comp])
+        return f
+
+    def N_e1n(self, k):
+        def f(r, theta, phi):
+            p = k*r
+            theta_comp = np.cos(phi)*self.tau_func(theta)*(self.z_func(p) + p*self.zp_func(p))/p
+            phi_comp = -1*np.sin(phi)*self.pi_func(theta)*(self.z_func(p) + p*self.zp_func(p))/p
+            r_comp = np.cos(phi)*self.n*(self.n+1)*np.sin(theta)*self.pi_func(theta)*self.z_func(p)/p
+            return np.array([r_comp, theta_comp, phi_comp])
+        return f
+
+def riccati_1_single(n,x):
+    """Riccati_1, but only a single n value"""
+    pre = (np.pi*x/2)**.5
+    jn = pre*special.jv(n+0.5,x)
+    jnp = jn/(2*x) + pre*special.jvp(n+0.5,x)
+
+    return np.array([jn,jnp])
+
+def riccati_2_single(n,x):
+    """Riccati_2, but only a single n value"""
+    pre = (np.pi*x/2)**.5
+    hn = pre*special.hankel1(n+0.5,x)
+    hnp = hn/(2*x) + pre*special.h1vp(n+0.5,x)
+
+    return np.array([hn,hnp])
+
 def riccati_3_single(n,x):
     """Riccati_3, but only a single n value"""
     # pre = (np.pi*x/2)**.5
@@ -66,3 +130,7 @@ def riccati_3_single(n,x):
     # return np.array([yn,ynp])
     return riccati_2_single(n,x) - riccati_1_single(n,x)
 
+
+if __name__ == "__main__":
+    theta = np.linspace(0,1,5)
+    print(pi_tau_func(3,theta))
