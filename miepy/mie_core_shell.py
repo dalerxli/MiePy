fix important factor of n_background in scattering calculation

miepy/mie_core_shell.py
@@ -71,4 +71,4 @@ def core_shell(nmax, mat_in, mat_out,  r_in, r_out, eps_b=1, mu_b=1):
     
     an = np.nan_to_num(an)
     bn = np.nan_to_num(bn)
-    return multipoles(mat_in.wav,an,bn) 
+    return multipoles(mat_in.wav, nb, an,bn) 

miepy/mie_sphere.py
@@ -30,6 +30,7 @@ class sphere:
         self.dn = np.zeros((nmax,mat.Nfreq), dtype=np.complex)
 
         self.exterior_computed = False
+        self.interior_computed = False
     
     def scattering(self):
         xvals = self.mat.k*self.r*self.n_b
@@ -49,7 +50,7 @@ class sphere:
         self.bn = np.nan_to_num(self.bn)
 
         self.exterior_computed = True
-        return multipoles(self.mat.wav,self.an,self.bn) 
+        return multipoles(self.mat.wav, self.n_b, self.an, self.bn) 
 
     def compute_cd(self):
         xvals = self.mat.k*self.r*self.n_b

miepy/scattering.py
@@ -29,12 +29,17 @@ class multipoles:
     """Contains an and bn as function of wavelength
        Used to calculate scattering/absorption properties"""
 
-    def __init__(self, wav, an, bn):
+    def __init__(self, wav, n_b,  an, bn):
+        """     wav[N]       wavelength
+                n_b          background index
+                an[N]        a scattering coefficients
+                bn[N]        b scattering coefficients    """
+
         self.Nfreq = len(wav)
         self.energy = wav_to_energy(wav)
         self.an = an
         self.bn = bn
-        self.k = 2*np.pi/wav
+        self.k = 2*np.pi*n_b/wav
 
     def mode_scattering(self, mtype, n):
         """Get modal scattering intensity