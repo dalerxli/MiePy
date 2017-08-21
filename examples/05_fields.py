b3bc14e121512a08558161a934abd83b1b1abbc6
John <japarker@uchicago.edu>
2017-08-21 03:07:23 -0500

    working E and H examples

--- examples/05_fields.py
+++ examples/05_fields.py
@@ -4,31 +4,25 @@ Displaying the fields in an xy cross section of the sphere (x polarized light, z
 
 import numpy as np
 import matplotlib.pyplot as plt
-from miepy.materials import material
-from miepy import single_mie_sphere
-import my_pytools.my_matplotlib.style as style
-import my_pytools.my_matplotlib.plots as plots
-import my_pytools.my_matplotlib.colors as colors
-
-style.screen()
+import miepy
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.cm as cm
 
 #wavelength from 400nm to 1000nm
-wav = np.linspace(400,1000,1000)
+wavelength = np.linspace(400e-9,1000e-9,1000)
 
-#create a material with n = 3.7 (eps = n^2) at all wavelengths
-eps = 0.5*np.ones(1000) + 0.1j
-mu = 1*np.ones(1000)
-dielectric = material(wav,eps,mu)     #material object
+Ag = miepy.materials.predefined.Ag()
+Ag = miepy.material_functions.constant_material(1.1)
 
 #calculate scattering coefficients
-rad = 20       # 100 nm radius
-Nmax = 5       # Use up to 10 multipoles
-m = single_mie_sphere(Nmax, dielectric, rad)
+radius = 20e-9       # 100 nm radius
+Lmax = 1       # Use up to 10 multipoles
+sphere = miepy.single_mie_sphere(radius, Ag, wavelength, Lmax)
 
-E_func = m.H_field(999)
-x = np.linspace(-2*rad,2*rad,200)
-y = np.linspace(-2*rad,2*rad,200)
-z = np.array([rad*0.0])
+E_func = sphere.E_field(index=999)
+x = np.linspace(-2*radius,2*radius,100)
+y = np.linspace(-2*radius,2*radius,100)
+z = np.array([radius*0.0])
 
 X,Y,Z = np.meshgrid(x,y,z, indexing='xy')
 R = (X**2 + Y**2 + Z**2)**0.5
@@ -37,13 +31,13 @@ PHI = np.arctan2(Y,X)
 PHI[PHI<0] += 2*np.pi
 # R, THETA, PHI = np.meshgrid(r,theta,phi, indexing='ij')
 
-E = -1*E_func(R,THETA,PHI)
+E = E_func(R,THETA,PHI)
 E = np.squeeze(E)
 I = np.sum(np.abs(E)**2, axis=0)
 # I /= np.max(I)
 
 fig = plt.figure()
-plt.pcolormesh(np.squeeze(X),np.squeeze(Y), I, shading="gouraud")
+plt.pcolormesh(np.squeeze(X),np.squeeze(Y), I, shading="gouraud", cmap=cm.viridis)
 plt.colorbar(label='electric field intensity')
 
 # plt.streamplot(np.squeeze(X), np.squeeze(Y), np.real(E[0]), np.real(E[1]), color='white')
@@ -53,22 +47,19 @@ PHI = np.squeeze(PHI)
 Ex = E[0]*np.sin(THETA)*np.cos(PHI) + E[1]*np.cos(THETA)*np.cos(PHI) - E[2]*np.sin(PHI)
 Ey = E[0]*np.sin(THETA)*np.sin(PHI) + E[1]*np.cos(THETA)*np.sin(PHI) + E[2]*np.cos(PHI)
 
-# Ex[np.squeeze(R) > rad] = 0
+# Ex[np.squeeze(R) > radius] = 0
 
 step=10
 # plt.quiver(np.squeeze(X)[::step,::step], np.squeeze(Y)[::step,::step], np.real(Ex)[::step,::step], np.real(Ey)[::step,::step], color='white')
 
 
-plt.streamplot(np.squeeze(X), np.squeeze(Y), np.real(Ex), np.real(Ey), color='white', linewidth=2)
+plt.streamplot(np.squeeze(X), np.squeeze(Y), np.real(Ex), np.real(Ey), color='black', linewidth=1.5)
 # plt.streamplot(np.squeeze(X), np.squeeze(Y), np.real(Ey.T), np.real(Ex.T), color='white', linewidth=2)
-plt.xlim([-2*rad,2*rad])
-plt.ylim([-2*rad,2*rad])
+plt.xlim([-2*radius,2*radius])
+plt.ylim([-2*radius,2*radius])
 plt.xlabel("X (nm)")
 plt.ylabel("Y (nm)")
-plots.set_num_ticks(5,5)
 plt.title(r'$\varepsilon = 4 + 0.1i$, $\lambda = 1 \mu m$' + '\n')
-
-plt.show()
 
 theta = np.linspace(0,np.pi,50)
 phi = np.linspace(0,2*np.pi,50)
@@ -89,19 +80,18 @@ I = np.squeeze(I)
 I -= np.min(I)
 I /= np.max(I)
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.cm as cm
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 shape = X.shape
 C = np.zeros((shape[0], shape[1], 4))
-cmap_3d = colors.cmap['parula']
+cmap_3d = cm.viridis
 for i in range(shape[0]):
     for j in range(shape[1]):
         # C[i,j,:] = matplotlib.cm.rainbow(R[i,j])
         C[i,j,:] = cmap_3d(I[i,j])
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,shade=False, facecolors=C,linewidth=.0, edgecolors='#000000', antialiased=False)
+surf = ax.plot_surface(X*1e9, Y*1e9, Z*1e9, rstride=1, cstride=1,shade=False, facecolors=C,linewidth=.0, edgecolors='#000000', antialiased=False)
 m = cm.ScalarMappable(cmap=cmap_3d)
 m.set_array(I)
 plt.colorbar(m)
@@ -109,6 +99,3 @@ surf.set_edgecolor('k')
 ax.set_xlabel('X')
 
 plt.show()
-
-plt.show()
-
--- miepy/mie_sphere.py
+++ miepy/mie_sphere.py
@@ -76,19 +76,18 @@ class single_mie_sphere:
 
     def solve_interior(self):
         """solve for the interior of the sphere, the cn and dn coefficients"""
-        xvals = self.mat.k*self.r*self.n_b
+        xvals = self.material_data['k']
+        m = (self.material_data['eps']/self.material_data['eps_b'])**.5
+        mt = m*self.material_data['mu_b']/self.material_data['mu']
         for i,x in enumerate(xvals):
-            m = (self.mat.eps[i]/self.eps_b)**.5
-            mt = m*self.mu_b/self.mat.mu[i]
+            jn,jn_p = riccati_1(self.Lmax,x)
+            jnm,jnm_p = riccati_1(self.Lmax,m[i]*x)
+            yn,yn_p = riccati_2(self.Lmax,x)
 
-            jn,jn_p = riccati_1(self.nmax,x)
-            jnm,jnm_p = riccati_1(self.nmax,m*x)
-            yn,yn_p = riccati_2(self.nmax,x)
-
-            c = (m*jn*yn_p - m*yn*jn_p)/(jnm*yn_p - mt*yn*jnm_p)
-            d = (m*jn*yn_p - m*yn*jn_p)/(mt*jnm*yn_p - yn*jnm_p)
-            self.cn[:,i] = c[1:]
-            self.dn[:,i] = d[1:]
+            c = (m[i]*jn*yn_p - m[i]*yn*jn_p)/(jnm*yn_p - mt[i]*yn*jnm_p)
+            d = (m[i]*jn*yn_p - m[i]*yn*jn_p)/(mt[i]*jnm*yn_p - yn*jnm_p)
+            self.cn[i] = c[1:]
+            self.dn[i] = d[1:]
         
         self.cn = np.nan_to_num(self.cn)
         self.dn = np.nan_to_num(self.dn)
@@ -109,30 +108,34 @@ class single_mie_sphere:
                 miepy.extinction_per_multipole(*self.scattering_properties))
 
 
-    def E_field(self, k_index, Nmax = None):
+    def E_field(self, index=None, Lmax=None):
         """Return an electric field function E(r,theta,phi) for a given wavenumber index"""
-        if not Nmax: Nmax = self.nmax
+        if Lmax is None: Lmax = self.Lmax
+        if index is None:
+            index = np.s_[:]
+
         if not self.interior_computed: self.solve_interior()
         if not self.exterior_computed: self.solve_exterior()
+        mat = self.material_data
 
         def E_func(r, theta, phi):
             E = np.zeros(shape = [3] + list(r.shape), dtype=np.complex)
-            id_inside = r <= self.r
+            id_inside = r <= self.radius
 
-            for n in range(1,Nmax+1):
+            for n in range(1,Lmax+1):
                 En = 1j**n*(2*n+1)/(n*(n+1))
 
-                an = self.an[n-1, k_index]
-                bn = self.bn[n-1, k_index]
-                cn = self.cn[n-1, k_index]
-                dn = self.dn[n-1, k_index]
+                an = self.an[index, n-1]
+                bn = self.bn[index, n-1]
+                cn = self.cn[index, n-1]
+                dn = self.dn[index, n-1]
 
-                k = self.mat.k[k_index]*self.eps_b**.5
+                k = mat['k'][index]
                 VSH = vector_spherical_harmonics(n,3)
                 E[:,~id_inside] += En*(1j*an*VSH.N_e1n(k)(r[~id_inside],theta[~id_inside],phi[~id_inside])  \
                                 - bn*VSH.M_o1n(k)(r[~id_inside],theta[~id_inside],phi[~id_inside]))
 
-                k = self.mat.k[k_index]*self.mat.eps[k_index]**.5
+                k = 2*np.pi*mat['n'][index]/self.wavelength[index]
                 VSH = vector_spherical_harmonics(n,1)
                 E[:,id_inside] += En*(cn*VSH.M_o1n(k)(r[id_inside],theta[id_inside],phi[id_inside])  \
                                 - 1j*dn*VSH.N_e1n(k)(r[id_inside],theta[id_inside],phi[id_inside]))
@@ -141,32 +144,37 @@ class single_mie_sphere:
 
         return E_func
 
-    def H_field(self, k_index, Nmax = None):
+    def H_field(self, index=None, Lmax=None):
         """Return a magnetic field function H(r,theta,phi) for a given wavenumber index"""
-        if not Nmax: Nmax = self.nmax
-        if not self.interior_computed: self.compute_cd()
-        if not self.exterior_computed: self.scattering()
+        if Lmax is None: Lmax = self.Lmax
+        if index is None:
+            index = np.s_[:]
+
+        if not self.interior_computed: self.solve_interior()
+        if not self.exterior_computed: self.solve_exterior()
+        mat = self.material_data
 
         def H_func(r, theta, phi):
             H = np.zeros(shape = [3] + list(r.shape), dtype=np.complex)
-            id_inside = r <= self.r
+            id_inside = r <= self.radius
 
-            for n in range(1,Nmax+1):
+            for n in range(1,Lmax+1):
                 En = 1j**n*(2*n+1)/(n*(n+1))
 
-                an = self.an[n-1, k_index]
-                bn = self.bn[n-1, k_index]
-                cn = self.cn[n-1, k_index]
-                dn = self.dn[n-1, k_index]
 
-                k = self.mat.k[k_index]*self.eps_b**.5
-                omega = self.mat.k[k_index]  # FIX THIS
+                an = self.an[index, n-1]
+                bn = self.bn[index, n-1]
+                cn = self.cn[index, n-1]
+                dn = self.dn[index, n-1]
+
+                k = mat['k'][index]
+                omega = 2*np.pi/self.wavelength[index]  # FIX THIS
                 VSH = vector_spherical_harmonics(n,3)
-                H[:,~id_inside] += k*En/(omega*self.mu_b)*(1j*bn*VSH.N_o1n(k)(r[~id_inside],theta[~id_inside],phi[~id_inside])  \
+                H[:,~id_inside] += k*En/(omega*mat['mu_b'][index])*(1j*bn*VSH.N_o1n(k)(r[~id_inside],theta[~id_inside],phi[~id_inside])  \
                                 + an*VSH.M_e1n(k)(r[~id_inside],theta[~id_inside],phi[~id_inside]))
 
-                k = self.mat.k[k_index]*self.mat.eps[k_index]**.5
-                mu = self.mat.mu[k_index]
+                k = 2*np.pi*mat['n'][index]/self.wavelength[index]
+                mu = mat['mu'][index]
                 VSH = vector_spherical_harmonics(n,1)
                 H[:,id_inside] += -k*En/(omega*mu)*(dn*VSH.M_e1n(k)(r[id_inside],theta[id_inside],phi[id_inside])  \
                                 + 1j*cn*VSH.N_o1n(k)(r[id_inside],theta[id_inside],phi[id_inside]))
